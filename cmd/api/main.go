145032f35da7241ead0411020d1ee4db5ebe39b1
arbazshaikh150 <arbazshaikh1507@gmail.com>
2026-08-16 19:07:12 +0530

    Configuration for the database is created successfully

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,30 @@
 package main
-import "fmt"
 
-func main(){
-	fmt.Print("Main is started")
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/arbazshaikh150/TimeCourt/internal/config"
+	"github.com/arbazshaikh150/TimeCourt/internal/database"
+)
+
+func main() {
+	// Making a postgress connection
+	config, err := config.Load()
+	context := context.Background()
+	if err != nil {
+		log.Fatal("Error Reading a database config from the environment variable", err)
+	}
+	// Making a connection to postgreSQL
+	fmt.Println("Establishing the postgres Connection")
+	db, err := database.NewPool(context, config.DatabaseURL)
+
+	if err != nil {
+		log.Fatal("Error in creating the database connection", err)
+	}
+	defer db.Close()
+
+	fmt.Println("Connection to postgress is successfull")
+
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,43 @@
 package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+// Creating the necessary config information
+type Config struct {
+	DatabaseURL string
+}
+
+func Load() (*Config, error) {
+	username := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	database := os.Getenv("DB_NAME")
+
+	if username == "" ||
+		password == "" ||
+		host == "" ||
+		port == "" ||
+		database == "" {
+		return nil, fmt.Errorf("database configuration is incomplete")
+	}
+
+	databaseURL := fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s",
+		username,
+		password,
+		host,
+		port,
+		database,
+	)
+
+	log.Println("Connection Url is created");
+
+	return &Config{
+		DatabaseURL: databaseURL,
+	}, nil
+}
